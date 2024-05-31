37342406174ad5d40e88eb12fc7b256972ede0b1
Marcos126 <natalemarcos06@gmail.com>
2024-05-31 13:32:17 -0300

    proyecto en movido a su propio repo

--- proyect/first_install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export DEBIAN_FRONTEND=noninteractive
-apt update && apt install sudo dpkg python3 python3-pip -y && pip install pwntools --break-system-packages
-python3 ~/documents/install.py
