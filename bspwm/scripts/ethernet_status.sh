33a333059f7e06dd47dbef27057c24ef27bb63ce
Marcos126 <natalemarcos06@gmail.com>
2024-06-01 04:48:15 -0300

    changing updating ethernet_status.sh

--- bspwm/scripts/ethernet_status.sh
+++ bspwm/scripts/ethernet_status.sh
@@ -1,24 +1,14 @@
 #!/bin/bash
 
-ifs=(usb0 eth0)
-ip_addresses=()
+search=$(hostname -I | sed s/\ /\\n/g | grep -v 172 | xargs )
 
-function usb(){
-    (/usr/sbin/ifconfig $is) &>/dev/null
-    if [ $? -eq 0 ];then
-        ip=$(/usr/sbin/ifconfig $is | grep "inet " | awk '{print $2}')
-        if [ -n "$ip" ]; then
-            ip_addresses+=("$ip")
-        fi
-    fi
-}
+ip_addresses=$(/usr/sbin/ifconfig | grep -B1 "$search" | grep -w inet | awk '{print $2}')
 
-for is in ${ifs[@]}; do
-    usb $is
-done
-
-if [ ${#ip_addresses} -eq 0 ] ; then
+if [ -z "$search" ];then
     echo %{F#2495e7}󰌙    %{F#ffffff} "Disconnected"%{u-}
 else 
     echo %{F#2495e7}󰈀    %{F#ffffff} "$ip_addresses"%{u-}
 fi
+ 
+
+
